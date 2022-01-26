49064f458daeaf2f3e8fbb92c2c7e56328c602e4
Shanille Misa <shanille808@yahoo.com>
2022-01-25 23:39:57 -0800

    added code

--- js/main.js
+++ js/main.js
@@ -14,3 +14,14 @@ function myFunction() {
         x.className = "topnav";
     }
 }  
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate querystring parameters
+const urlParams = new URLSearchParams(queryString);
+urlParams.forEach(function(value, key) {
+    console.log(value, key);
+  });
